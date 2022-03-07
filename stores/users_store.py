966d91b3de3a5b19efbb2d6054a999eabd59f019
Noy <noyla00@gmail.com>
2022-03-07 23:05:43 +0200

    Upsert users & user reserves

--- stores/users_store.py
+++ stores/users_store.py
@@ -1,7 +1,8 @@
 from db.engine import session, engine
 from models.db.user import User
 from models.db.user_reserve_data import UserReserveData
-from sqlalchemy import text, insert
+from sqlalchemy import text
+from sqlalchemy.dialects.mysql import insert
 
 class UsersStore:
     @staticmethod
@@ -13,20 +14,17 @@ class UsersStore:
         session.add_all(users)
     
     @staticmethod
-    def create_users_with_reserves(users: list[User], 
+    def create_users_with_reserves(users: dict[User], 
                 user_reserves: list[UserReserveData]):
         users = [u.to_dict() for u in users.values()]
         user_reserves = [u.to_dict() for u in user_reserves]
-        insert_users_stmt = insert(User).values(users).prefix_with('IGNORE')
-        insert_user_reserves_stmt = insert(UserReserveData).values(user_reserves).\
-            prefix_with('IGNORE')
-        # no_update_stmt = stmt.
+
+        # Upsert users & user reserves
+        users_stmt = User.upsert(users)
+        user_reserves_stmt = UserReserveData.upsert(user_reserves)
+
         # compiled = stmt.compile()
         with engine.connect() as conn:
-            res = conn.execute(insert_users_stmt)
-            res = conn.execute(insert_user_reserves_stmt)
-            # conn.commit()
+            res = conn.execute(users_stmt)
+            res = conn.execute(user_reserves_stmt)
 
-
-            # conn.execute(text(f"INSERT INTO {User.__tablename__}") VALUES {values}"
-            # , {"some_private_name": "pii"})
